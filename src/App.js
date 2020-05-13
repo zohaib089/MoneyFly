bcbe803d3e197c5adebd93a89b0bf3abe90e90f2
zohaib089 <zabi.jarral08@gmail.com>
2020-05-13 06:33:20 +0200

    setup the basic structure to start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <h1>Hello from Moneyfly</h1>
+
+    </div>
+  );
+};
+
+
+
+
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
